a3acd853eb409f09f484ba2145da0764ef5525c6
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2025-03-04 14:15:13 +0100

    Refactor get_processed_dataset function to include split parameter for optional dataset splitting

--- src/data/process_datasets.py
+++ src/data/process_datasets.py
@@ -2,12 +2,13 @@ from sklearn.model_selection import train_test_split
 
 from src.data.load_datasets import load_sentiment
 
-def get_processed_dataset(dataset, test_size=0.2, random_state=42):
+def get_processed_dataset(dataset, split: bool, test_size=0.2, random_state=42):
     """
     Splits the dataset into training and test sets.
 
     Args:
         dataset: The dataset to split. Available options: "sentiment".
+        split: Whether to split the dataset into training and test sets. Default is True.
         test_size: The proportion of the dataset to include in the test split. Default is 0.2.
         random_state: Controls the shuffling applied to the data before applying the split. Default is 42.
 
@@ -16,6 +17,8 @@ def get_processed_dataset(dataset, test_size=0.2, random_state=42):
     """
     if dataset == "sentiment":
         dataset = load_sentiment()
+        if not split:
+            return dataset["sentence"], dataset["label"]
         train_sentences, test_sentences, train_labels, test_labels = train_test_split(dataset["sentence"], dataset["label"], test_size=test_size, random_state=random_state)
         return train_sentences, test_sentences, train_labels, test_labels
     else:
