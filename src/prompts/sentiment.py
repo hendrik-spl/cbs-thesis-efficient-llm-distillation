a529b5699fa67136e91a696b852bd990de23130d
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2025-03-15 16:50:12 +0100

    implemented original sentiment prompt

--- src/prompts/sentiment.py
+++ src/prompts/sentiment.py
@@ -1,11 +1,22 @@
 def get_sentiment_prompt(text):
-    return f"""
-            Please classify the sentiment of the following financial text:
-            {text}
-            Sentiment Options: negative, neutral, positive.
-            Respond with the integer corresponding to the sentiment.
-            0: negative
-            1: neutral
-            2: positive
-            Do not include any other information in your response.
-            """
+    return f"""\
+You are a highly qualified expert trained to annotate machine learning training data.
+
+Your task is to analyze the sentiment in the TEXT below from an investor perspective and label it with only one the three labels:
+positive, negative, or neutral.
+
+Base your label decision only on the TEXT and do not speculate e.g. based on prior knowledge about a company. 
+
+Do not provide any explanations and only respond with one of the labels as one word: negative, positive, or neutral
+
+Examples:
+Text: Operating profit increased, from EUR 7m to 9m compared to the previous reporting period.
+Label: positive
+Text: The company generated net sales of 11.3 million euro this year.
+Label: neutral
+Text: Profit before taxes decreased to EUR 14m, compared to EUR 19m in the previous period.	
+Label: negative
+
+Your TEXT to analyse:
+TEXT: {text}
+Label: """
