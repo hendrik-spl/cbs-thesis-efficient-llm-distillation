4b1854b75a504f50c696171a73ac8335bcdff1a4
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2025-04-29 08:47:05 +0200

    added additional stop word for summary stopping criteria

--- src/models/hf_utils.py
+++ src/models/hf_utils.py
@@ -107,7 +107,7 @@ class HF_Manager:
                 KeywordStoppingCriteria(tokenizer, stop_words, prompt_length, max_tokens=3)
             ])
         elif "summary" in dataset_name:
-            stop_words = ["please let me know if"]
+            stop_words = ["please let me know if", "i hope it is correct"]
             stopping_criteria = StoppingCriteriaList([
                 KeywordStoppingCriteria(tokenizer, stop_words, prompt_length, max_tokens=None)
             ])
